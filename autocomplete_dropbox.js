247fe5611fe189e21e1d818adff78f44be883585
Willem Coetzee <williecoetzee@gmail.com>
2013-02-12 10:19:36 +0000

    Cleaned up js code.

--- autocomplete_dropbox.js
+++ autocomplete_dropbox.js
@@ -9,138 +9,137 @@
  * Using Drupal behaviours to declare main function
  */
 Drupal.behaviors.autocomplete_dropbox = function() {
-  //Workaround for dropdown not going away when clicking elsewhere on the screen
-  $("body").click(function(e) {
-    if(!((e.target.id == "term-names-wrapper-0") || (e.target.id == "term-names-input-0") || ((e.target.id == "term-names-dropdown-item-0")))){
-        $(".dropdown-term-names").hide();
-        $(".term-names-input").val("");
-      }
-  });
-
-  $('.term-names-wrapper').keydown(function(e) {
-    if(e.keyCode == 13) {
-      e.preventDefault();
-      return false;
-    }
-    if((e.keyCode == 9) && ($(".term-names-input").is(":focus"))) {
-      e.preventDefault();
-      return false;
-    }
-  });
-
-  // Set term counter
-  $('.autocomplete-dropbox-field-wrapper').each(function() {
-    $(this).find('.autocomplete-dropbox-label .terms-count .entered').html($(this).find('#entered-term-names-wrapper').children().length);
-  });
-
-
-  // Support for Tipsy. If Tipsy module installed, add tipsy.
-  try {
-    $('.term-names-wrapper').tipsy({fade: true, gravity: $.fn.tipsy.autoWE, delayIn: 0, delayOut: 500, trigger: 'hover', opacity: 0.8, offset: 0, title: function() { return $(this).find('.description').html();}, html: 1});
-  } catch(e) {}
-
-
-  function getDropdownData(vocabId) {
-
-    //var dropdownData = [];
-    //$.ajax({
-    //  url: "http://dev.nojoshmo.com/autocomplete_dropbox.json?vid="+vocabId,
-    //  async: false,
-    //  dataType: 'json',
-    //  success: function (json) {
-    //    data = json;
-    //    dropdownData = new Array();
-    //    for(var i in data) {
-    //      var innerArray = new Array();
-    //      innerArray["id"] = data[i].tid;
-    //      innerArray["value"] = data[i].name;
-    //      dropdownData[i] = innerArray;
-    //    }
-    //  }
-    //});
-    //return dropdownData;
-    return Drupal.settings.autocomplete_dropbox["a"+vocabId];
+//Workaround for dropdown not going away when clicking elsewhere on the screen
+$("body").click(function(e) {
+  if(!((e.target.id == "term-names-wrapper-0") || (e.target.id == "term-names-input-0") || ((e.target.id == "term-names-dropdown-item-0")))){
+    $(".dropdown-term-names").hide();
+    $(".term-names-input").val("");
   }
-  /**
-   * Get the term value by Id.
-   *
-   * A helper function to get the term value by id or hash preceded value.
-   *
-   * @param termId: The term's id or something like this... ###brown.
-   * @param data: The data array in the same format as getDropDownData function's return value.
-   *
-   * @return: The value of the term if it exists, oherwise returns false.
-   */
-  function getTermValue(termId, data) {
-    for (var k = 0; k < data.length; k++) {
-      if( data[k]['id'] == termId )
-            return data[k]['value'];
-    }
+});
+
+$('.term-names-wrapper').keydown(function(e) {
+  if(e.keyCode == 13) {
+    e.preventDefault();
     return false;
   }
+  if((e.keyCode == 9) && ($(".term-names-input").is(":focus"))) {
+    e.preventDefault();
+    return false;
+  }
+});
 
-  var navigationIndex = 0;
+// Set term counter
+$('.autocomplete-dropbox-field-wrapper').each(function() {
+  $(this).find('.autocomplete-dropbox-label .terms-count .entered').html($(this).find('#entered-term-names-wrapper').children().length);
+});
 
-  $('.term-names-wrapper').each(function() {
-    $(this).find('.description').html($(this).parent().find('.textfield').find('.description').html());
-    var ids = $(this).parent().find('.form-text').val().split(',');
-    if ( $(this).find('#entered-term-names-wrapper').find('.entered-term-name').length !=  ids.length) {
-      $(this).find('#entered-term-names-wrapper').html('');
-      for (var i = 0; i < ids.length; i++) {
-        var termValue;
-        if (ids[i].substr(0,1) == '#')
-          termValue = ids[i].substr(3);
-        else
-          termValue = getTermValue(ids[i], getDropdownData($(this).parent().find('.vocab-id').html()));
 
-        if (termValue)
-          $(this).find('#entered-term-names-wrapper').append('<div class="entered-term-name"><div class="term-id">' + ids[i] + '</div>' + termValue + '<a class="close-entered-term-name">×</a></div>');
-      }
+// Support for Tipsy. If Tipsy module installed, add tipsy.
+try {
+  $('.term-names-wrapper').tipsy({fade: true, gravity: $.fn.tipsy.autoWE, delayIn: 0, delayOut: 500, trigger: 'hover', opacity: 0.8, offset: 0, title: function() { return $(this).find('.description').html();},   html: 1});
+} catch(e) {}
+
+function getDropdownData(vocabId) {
+
+  //var dropdownData = [];
+  //$.ajax({
+  //  url: "http://dev.nojoshmo.com/autocomplete_dropbox.json?vid="+vocabId,
+  //  async: false,
+  //  dataType: 'json',
+  //  success: function (json) {
+  //    data = json;
+  //    dropdownData = new Array();
+  //    for(var i in data) {
+  //      var innerArray = new Array();
+  //      innerArray["id"] = data[i].tid;
+  //      innerArray["value"] = data[i].name;
+  //      dropdownData[i] = innerArray;
+  //    }
+  //  }
+  //});
+  //return dropdownData;
+  return Drupal.settings.autocomplete_dropbox["a"+vocabId];
+}
+  
+/**
+ * Get the term value by Id.
+ *
+ * A helper function to get the term value by id or hash preceded value.
+ *
+ * @param termId: The term's id or something like this... ###brown.
+ * @param data: The data array in the same format as getDropDownData function's return value.
+ *
+ * @return: The value of the term if it exists, oherwise returns false.
+ */
+function getTermValue(termId, data) {
+  for (var k = 0; k < data.length; k++) {
+    if( data[k]['id'] == termId )
+      return data[k]['value'];
+  }
+  return false;
+}
+
+var navigationIndex = 0;
+
+$('.term-names-wrapper').each(function() {
+  $(this).find('.description').html($(this).parent().find('.textfield').find('.description').html());
+  var ids = $(this).parent().find('.form-text').val().split(',');
+  if ( $(this).find('#entered-term-names-wrapper').find('.entered-term-name').length !=  ids.length) {
+    $(this).find('#entered-term-names-wrapper').html('');
+    for (var i = 0; i < ids.length; i++) {
+      var termValue;
+      if (ids[i].substr(0,1) == '#')
+        termValue = ids[i].substr(3);
+      else
+        termValue = getTermValue(ids[i], getDropdownData($(this).parent().find('.vocab-id').html()));
+
+      if (termValue)
+        $(this).find('#entered-term-names-wrapper').append('<div class="entered-term-name"><div class="term-id">' + ids[i] + '</div>' + termValue + '<a class="close-entered-term-name">×</a></div>');
     }
-  });
+  }
+});
 
-  // The delete button next to each term
-  $(".entered-term-name a").click(function() {
-    var temp_array = $(this).parent().parent().parent().parent().find('.form-text').val().split(',');
-    var deleteIndex = 0;
-    for (var i = 0; i < temp_array.length; i++) {
-      if ((temp_array[i] == $(this).parent().children('.term-id').html()) || (temp_array[i] == '###' + $(this).parent().children('.new-item').html()) )
-        deleteIndex = i;
-    }
-    temp_array.splice(deleteIndex,1);
+// The delete button next to each term
+$(".entered-term-name a").click(function() {
+  var temp_array = $(this).parent().parent().parent().parent().find('.form-text').val().split(',');
+  var deleteIndex = 0;
+  for (var i = 0; i < temp_array.length; i++) {
+    if ((temp_array[i] == $(this).parent().children('.term-id').html()) || (temp_array[i] == '###' + $(this).parent().children('.new-item').html()) )
+      deleteIndex = i;
+  }
+  temp_array.splice(deleteIndex,1);
 
-    //Now we use this code to make a string again since join doesnt want to work
-    if (temp_array.length == 1) {
-      var temp_string = temp_array[0];
+  //Now we use this code to make a string again since join doesnt want to work
+  if (temp_array.length == 1) {
+    var temp_string = temp_array[0];
+  }
+  else if (temp_array.length > 1) {
+    var temp_string = '';
+    for (var k = 0; k < temp_array.length; k++) {
+      if (k == 0)
+        temp_string = temp_array[k];
+      else
+        temp_string = temp_string + ',' + temp_array[k];
     }
-    else if (temp_array.length > 1) {
-      var temp_string = '';
-      for (var k = 0; k < temp_array.length; k++) {
-        if (k == 0)
-          temp_string = temp_array[k];
-        else
-          temp_string = temp_string + ',' + temp_array[k];
-      }
-    }
-    else {
-      temp_string = '';
-    }
-    $(this).parent().parent().parent().parent().find('.terms-count .entered').html($(this).parent().parent().children().length - 1);
-    $(this).parent().parent().parent().parent().find('.form-text').val(temp_string);
-    $(this).parent().remove();
-  });
+  }
+  else {
+    temp_string = '';
+  }
+  $(this).parent().parent().parent().parent().find('.terms-count .entered').html($(this).parent().parent().children().length - 1);
+  $(this).parent().parent().parent().parent().find('.form-text').val(temp_string);
+  $(this).parent().remove();
+});
 
-  $(".term-names-wrapper").click(function(e) {
-
-    $(this).find(".term-names-input").focus();
-      if($(this).parent().parent().find(".term-names-dropdown").is(":visible")) {
-      //do nothing...
-      }
-      else {
-        $(".dropdown-term-names").hide();
-        $(".term-names-input").val("");
-      }
-  });
+$(".term-names-wrapper").click(function(e) {
+  $(this).find(".term-names-input").focus();
+  if($(this).parent().parent().find(".term-names-dropdown").is(":visible")) {
+    //do nothing...
+  }
+  else {
+    $(".dropdown-term-names").hide();
+    $(".term-names-input").val("");
+  }
+});
 
   $(".term-names-input").keyup(function(e, keyCode){
 
@@ -266,13 +265,8 @@ Drupal.behaviors.autocomplete_dropbox = function() {
         $(this).parent().css("background-image", "url("+Drupal.settings['module_path']['0']+"/images/throbber.gif)");
       }
     }
-    // Backspace pressed
-    if (keyCode == 8) {
-      // Leave for later
-    }
 
     // Mouse actions
-
     // The delete button next to each entered term
     $(this).parent().find(".entered-term-name a").click(function() {
       var temp_array = $(this).parent().parent().parent().parent().find('.form-text').val().split(',');
